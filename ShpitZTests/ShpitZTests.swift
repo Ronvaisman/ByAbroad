85fda12eca59c27d7dbcde5eb884d4a02cdce232
Ron Vaisman <ron.vaisman@example.com>
2025-07-22 14:42:32 +0300

    Initial Commit

---
+++ ShpitZTests/ShpitZTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShpitZTests.swift
+//  ShpitZTests
+//
+//  Created by Ron Vaisman on 22/07/2025.
+//
+
+import Testing
+@testable import ShpitZ
+
+struct ShpitZTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
